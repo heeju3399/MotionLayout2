f04b9ddffdf25cdffa9dba20d4f875ff5d41de2f
heeju3399 <heeju3399@naver.com>
2020-08-29 14:42:41 +0900

    ok

---
+++ app/src/main/java/org/techtown/sssssssssssex1/MainActivity.java
@@ -0,0 +1,18 @@
+package org.techtown.sssssssssssex1;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.WindowDecorActionBar;
+
+import android.app.ActionBar;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle("MotionLayout");
+
+    }
+}
